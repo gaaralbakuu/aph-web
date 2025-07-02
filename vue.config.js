bb64dbbf0687fe7eedc5f3b77d6edfeecdc832b0
Gaaralbakuu <Dat-PhamMinh@vn.apachefootwear.com>
2025-07-02 13:14:06 +0700

    modify build

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ process.env.VUE_APP_API = 'http://10.30.3.121:65301' //prd
 
 if (process.env.NODE_ENV === 'development') {
   process.env.VUE_APP_API = 'http://10.30.2.247:5000' //test
-  process.env.VUE_APP_API = 'http://localhost:65301' //prd
+  // process.env.VUE_APP_API = 'http://localhost:65301' //prd
 }
 
 if (process.env.NODE_ENV === 'production') {
