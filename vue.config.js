3bf0de078f4d09e2eea80d52e4e59269c796bd3d
Gaaralbakuu <Dat-PhamMinh@vn.apachefootwear.com>
2025-07-15 15:17:38 +0700

    update url

--- vue.config.js
+++ vue.config.js
@@ -6,8 +6,7 @@ const resolve = (dir) => require('path').join(__dirname, dir)
   process.env.VUE_APP_API = 'http://localhost:65301' //prd
 
 if (process.env.NODE_ENV === 'development') {
-  // process.env.VUE_APP_API = 'http://10.30.2.247:5000' //test
-  // process.env.VUE_APP_API = 'http://localhost:65301' //prd
+  process.env.VUE_APP_API = 'http://10.30.2.247:5000' //test
 }
 
 if (process.env.NODE_ENV === 'production') {
