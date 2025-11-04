bfd829ff410b1613ef61341af035a87d9ccee0f9
Gaaralbakuu <Dat-PhamMinh@vn.apachefootwear.com>
2025-11-04 13:12:37 +0700

    hidden issue type

--- src/views/sysbasics/compliance/improveManagement.vue
+++ src/views/sysbasics/compliance/improveManagement.vue
@@ -17,14 +17,14 @@
           </div>
           <input type="text" :placeholder="$l.manufactureName" v-model="formInline.manufacture_name" class="h-9 pr-3 pl-10 border border-gray-200 rounded-full focus:outline-none dark:bg-gray-800 dark:border-gray-700 dark:text-white" />
         </div>
-        <div class="relative h-9">
+        <!-- <div class="relative h-9">
           <div class="absolute left-3 top-1/2 transform -translate-y-1/2 pointer-events-none">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="w-5 h-5">
               <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
             </svg>
           </div>
           <input type="text" :placeholder="$l.issueType" v-model="formInline.issue_type" class="h-9 pr-3 pl-10 border border-gray-200 rounded-full focus:outline-none dark:bg-gray-800 dark:border-gray-700 dark:text-white" />
-        </div>
+        </div> -->
       </div>
       
 
