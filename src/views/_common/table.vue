e6c3a157bac65806aa6612c774ef47959ec59e87
Gaaralbakuu <Dat-PhamMinh@vn.apachefootwear.com>
2025-12-05 14:33:25 +0700

    modify table

--- src/views/_common/table.vue
+++ src/views/_common/table.vue
@@ -1,53 +1,24 @@
 <template>
   <div>
     <slot name="research"></slot>
-    <div class="overflow-x-auto border border-gray-200 rounded-lg">
-      <table class="w-full border-collapse">
-        <thead class="bg-gray-50">
-          <tr class="border-b border-gray-200">
-            <th v-for="(item, index) in columns" :key="index" 
-                class="px-4 py-3 text-left text-sm font-semibold text-gray-900 truncate"
-                :title="item.title || item.key">
-              {{ item.title || item.key }}
-            </th>
-            <th v-if="!tableProps.hideOperations" 
-                class="px-4 py-3 text-left text-sm font-semibold text-gray-900 w-24">
-              {{ $c.operation }}
-            </th>
-          </tr>
-        </thead>
-        <tbody class="divide-y divide-gray-200">
-          <tr v-if="!list || list.length === 0" class="hover:bg-gray-50">
-            <td :colspan="(columns ? columns.length : 0) + (tableProps.hideOperations ? 0 : 1)" 
-                class="px-4 py-8 text-center text-gray-500">
-              {{ $c.no_data || 'No data available' }}
-            </td>
-          </tr>
-          <tr v-for="(row, rowIndex) in list" :key="row.id || rowIndex" 
-              class="hover:bg-gray-50 transition-colors cursor-pointer"
-              @click="rowClick(row)">
-            <td v-for="(item, colIndex) in columns" :key="colIndex" 
-                class="px-4 py-3 text-sm text-gray-700 border-b border-gray-100 truncate"
-                :title="row[item.key]">
-              <slot name="content" v-bind="{row:row,key:item.key,index:rowIndex}">
-                <span>{{ row[item.key] }}</span>
-              </slot>
-            </td>
-            <td v-if="!tableProps.hideOperations" 
-                class="px-4 py-3 text-sm text-gray-700 border-b border-gray-100 whitespace-nowrap">
-              <slot name="operation" v-bind="{row:row,$index:rowIndex}">
-                <a href="#" class="text-blue-600 hover:text-blue-800 mr-3" @click.prevent="editItem(row,rowIndex)">
-                  {{ $c.edit }}
-                </a>
-                <a href="#" class="text-red-600 hover:text-red-800" @click.prevent="deleteItem(row,rowIndex)">
-                  {{ $c.delete }}
-                </a>
-              </slot>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <el-table :data="list" row-key="id" v-bind="tableProps" @current-change="currentChange" @row-click="rowClick">
+      <el-table-column v-for="(item, index) in columns" :key="index" show-overflow-tooltip
+        :label="item.title || item.key" :prop="item.key" v-bind="item">
+        <template slot-scope="scope">
+          <slot name="content" v-bind="{row:scope.row,key:item.key,index:scope.$index}">
+            <span>{{scope.row[item.key]}}</span>
+          </slot>
+        </template>
+      </el-table-column>
+      <el-table-column v-if="!tableProps.hideOperations" :label="$c.operation" :width="tableProps.opsColWith||86" fixed="right">
+        <template slot-scope="scope">
+          <slot name="operation" v-bind="{row:scope.row,$index:scope.$index}">
+            <a href="#" class="text-blue" @click.prevent="editItem(scope.row,scope.$index)">{{ $c.edit }}</a>&nbsp;
+            <a href="#" class="text-red" @click.prevent="deleteItem(scope.row,scope.$index)">{{ $c.delete }}</a>
+          </slot>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -68,6 +39,9 @@ export default {
     columns: { type: Array, default: null },
   },
   methods: {
+    currentChange(row) {
+      this.$emit('current-change', row)
+    },
     rowClick(row) {
       this.$emit('row-click', row)
     },
